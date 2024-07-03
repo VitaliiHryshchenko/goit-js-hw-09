5e879f07a273baf95262c9d3ea3ab370dc88ff4d
VitaliiHryshchenko <v.grischenko80@gmail.com>
2024-07-03 15:03:20 +0200

    form complete

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,23 +1,5 @@
 const sectionForm = document.querySelector('.feedback-form');
-const inputUserEmail = document.querySelector('.js-input-form-email');
-const textareaUserComent = document.querySelector('.js-textarea-form-massage');
-const btn = document.querySelector('.js-form-btn');
-const LS_KEY = 'feedback-form-state';
-const formData = {
-  email: '',
-  messadge: '',
-};
 
-inputUserEmail.addEventListener('input', handlerInputForm);
-sectionForm.addEventListener('submit', handlerSubmitForm);
-function handlerInputForm(evt) {
-  console.log(evt.currentTarget);
-}
-function handlerSubmitForm(evt) {
-  evt.preventDefault();
-  const current = evt.target.value;
-  console.log(current);
-}
 const form = `<label>
     <span class="text-label">Email</span>
     <input class="js-input-form-email" type="email" name="email" autofocus />
@@ -28,3 +10,56 @@ const form = `<label>
   </label>
   <button class="js-form-btn" type="submit">Submit</button>`;
 sectionForm.insertAdjacentHTML('beforeend', form);
+const formData = {
+  email: '',
+  message: '',
+};
+
+sectionForm.addEventListener('input', handlerInput);
+
+function handlerInput(event) {
+  event.preventDefault();
+
+  if (event.target.name === 'email') {
+    formData.email = event.target.value.trim();
+  }
+
+  if (event.target.name === 'message') {
+    formData.message = event.target.value.trim();
+  }
+
+  localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+}
+
+const emailInput = document.getElementsByName('email');
+const messageInput = document.getElementsByName('message');
+const savedData = localStorage.getItem('feedback-form-state');
+
+function fillForm() {
+  if (savedData) {
+    const parsedData = JSON.parse(savedData);
+    formData.email = parsedData.email;
+    formData.message = parsedData.message;
+    emailInput[0].value = formData.email;
+    messageInput[0].value = formData.message;
+  }
+}
+
+fillForm();
+
+sectionForm.addEventListener('submit', handlerSubmit);
+
+function handlerSubmit(event) {
+  event.preventDefault();
+
+  if (!formData.email || !formData.message) {
+    alert('Fill please all fields');
+    return;
+  }
+
+  console.log(formData);
+  localStorage.removeItem('feedback-form-state');
+  formData.email = '';
+  formData.message = '';
+  sectionForm.reset();
+}
